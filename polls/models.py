86f607639d080b26bdf67f0143cf2eae43eda813
Zabih Azimy <63865512+zabiAzimy@users.noreply.github.com>
2023-06-01 17:41:38 +0200

    First commit

---
+++ polls/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    # adding the __str__()
+    def __str__(self):
+        return self.question_text
+
+    # adding a custom method to this model
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    # add __str__() here too
+    def __str__(self):
+        return self.choice_text
